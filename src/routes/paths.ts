6afe0a58900c50f331e11e618794f977e4c68b77
KaineBismarck <kaine.bismarck@appquest.com.ng>
2023-11-30 05:55:10 +0100

    term deposit summary UI added

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,5 +1,6 @@
 export const paths = {
   INVESTMENT_DASHBOARD: "/investment-management",
   INVESTMENT_TEST: "/investment-management/investment-test",
-  CREATE_TERM_DEPOSIT: "/investment-management/create-term-deposit",
+  CREATE_TERM_DEPOSIT: "/investment-management/term-deposit/create",
+  TERM_DEPOSIT_SUMMARY: "/investment-management/term-deposit/process-summary",
 };
