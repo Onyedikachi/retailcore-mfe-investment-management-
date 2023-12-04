a3c3ec78890577778034dab70eb8bb3b5e6e5e83
KaineBismarck <kaine.bismarck@appquest.com.ng>
2023-12-04 03:22:36 +0100

    updated UI for account entries and events

--- src/components/pages/term-deposit/forms/entries-and-events-search-results.tsx
+++ src/components/pages/term-deposit/forms/entries-and-events-search-results.tsx
@@ -6,12 +6,18 @@ export default function EntriesAndEventsSearchResults() {
       name: "Assets",
       menu: [
         {
-          name: "menu name",
-          subMenu: [{ name: "subMenu name" }],
+          name: "Current Assets [ASTCAS]",
+          subMenu: [
+            { name: "Current Account balances [ASTCAS23421]" },
+            { name: "Savings Account balances [ASTCAS23422]" },
+            { name: "Cash Receipt balances [ASTCAS23423]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+          ],
           isOpen: false,
         },
         {
-          name: "menu name2",
+          name: "Non Current Asets",
           subMenu: [{ name: "subMenu name2" }],
           isOpen: false,
         },
@@ -21,8 +27,19 @@ export default function EntriesAndEventsSearchResults() {
       name: "Liabilities",
       menu: [
         {
-          name: "menu name",
-          subMenu: [{ name: "subMenu name" }],
+          name: "Current Liabilities [ASTCAS]",
+          subMenu: [
+            { name: "Current Account balances [ASTCAS23421]" },
+            { name: "Savings Account balances [ASTCAS23422]" },
+            { name: "Cash Receipt balances [ASTCAS23423]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+          ],
+          isOpen: false,
+        },
+        {
+          name: "Non Current Liabilities",
+          subMenu: [{ name: "subMenu name2" }],
           isOpen: false,
         },
       ],
@@ -31,8 +48,19 @@ export default function EntriesAndEventsSearchResults() {
       name: "Equities",
       menu: [
         {
-          name: "menu name",
-          subMenu: [{ name: "subMenu name" }],
+          name: "Current Equities [ASTCAS]",
+          subMenu: [
+            { name: "Current Account balances [ASTCAS23421]" },
+            { name: "Savings Account balances [ASTCAS23422]" },
+            { name: "Cash Receipt balances [ASTCAS23423]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+          ],
+          isOpen: false,
+        },
+        {
+          name: "Non Current Equities",
+          subMenu: [{ name: "subMenu name2" }],
           isOpen: false,
         },
       ],
@@ -41,8 +69,19 @@ export default function EntriesAndEventsSearchResults() {
       name: "Revenues",
       menu: [
         {
-          name: "menu name",
-          subMenu: [{ name: "subMenu name" }],
+          name: "Current Revenues [ASTCAS]",
+          subMenu: [
+            { name: "Current Account balances [ASTCAS23421]" },
+            { name: "Savings Account balances [ASTCAS23422]" },
+            { name: "Cash Receipt balances [ASTCAS23423]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+          ],
+          isOpen: false,
+        },
+        {
+          name: "Non Current Revenues",
+          subMenu: [{ name: "subMenu name2" }],
           isOpen: false,
         },
       ],
@@ -51,8 +90,19 @@ export default function EntriesAndEventsSearchResults() {
       name: "Expenses",
       menu: [
         {
-          name: "menu name",
-          subMenu: [{ name: "subMenu name" }],
+          name: "Current Expenses [ASTCAS]",
+          subMenu: [
+            { name: "Current Account balances [ASTCAS23421]" },
+            { name: "Savings Account balances [ASTCAS23422]" },
+            { name: "Cash Receipt balances [ASTCAS23423]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+            { name: "Current Account balances [ASTCAS23424]" },
+          ],
+          isOpen: false,
+        },
+        {
+          name: "Non Current Expenses",
+          subMenu: [{ name: "subMenu name2" }],
           isOpen: false,
         },
       ],
@@ -60,13 +110,26 @@ export default function EntriesAndEventsSearchResults() {
   ];
 
   const [currentTab, setCurrentTab] = useState(tabLinks[0]);
+
+  const [menus, setMenus] = useState(currentTab.menu);
+
+  const toggleSubMenu = (menuIndex) => {
+    //get the index of the menu on click
+    setMenus((prevMenus) => {
+      const updatedMenus = [...prevMenus];
+      updatedMenus[menuIndex].isOpen = !updatedMenus[menuIndex].isOpen;
+      return updatedMenus;
+    });
+  };
+
   return (
-    <div className="w-[360px]">
+    <div className="w-full shadow">
       <div className="rounded-b-lg bg-[#fff p-6 w-full">
         <div className="flex flex-col">
           <div className="flex justify-between mb-3">
             {tabLinks.map((item) => (
               <div
+                key={item.name}
                 onClick={() => setCurrentTab(item)}
                 className="flex flex-col cursor-pointer"
               >
@@ -87,18 +150,18 @@ export default function EntriesAndEventsSearchResults() {
               </div>
             ))}
           </div>
-          <div className="">
-            {/* {currentTab} */}
-            {currentTab.menu.map((menu) => (
-              <div key={menu.name}>
+          <div className="max-h-[233px] overflow-y-auto flex flex-col gap-4 py-2">
+            {currentTab.menu.map((menu, index) => (
+              <div key={menu.name} className="text-sm text-[#636363]">
                 <div
-                  className="flex gap-[11px]"
+                  className="flex items-center gap-[11px]"
                   onClick={() => {
-                    menu.isOpen = !menu.isOpen;
-                    // console.log(menu.isOpen);
+                    toggleSubMenu(index);
                   }}
                 >
-                  <span>
+                  <span
+                    className={`${menu.isOpen ? "transform rotate-45" : ""}`}
+                  >
                     <svg
                       width="15"
                       height="16"
@@ -112,17 +175,13 @@ export default function EntriesAndEventsSearchResults() {
                       />
                     </svg>
                   </span>
-                  {menu.name} {menu.isOpen}
+                  {menu.name}
                 </div>
-                <div className="ml-[40px]">
-                  {menu.subMenu.map((subMenu) => (
-                    <span>{subMenu.name}</span>
-                  ))}
-                </div>
+
                 {menu.isOpen && (
-                  <div className="ml-[40px]">
+                  <div className="ml-[40px] flex flex-col gap-4">
                     {menu.subMenu.map((subMenu) => (
-                      <span>{subMenu.name}</span>
+                      <p key={subMenu.name}>{subMenu.name}</p>
                     ))}
                   </div>
                 )}
