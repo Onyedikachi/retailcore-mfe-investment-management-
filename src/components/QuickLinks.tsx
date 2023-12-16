232450ea536a510bc62d37d4bafb56d3fb527ef9
TOLULOPE <tolu.oduro@appquest.com.ng>
2023-12-16 08:55:04 +0100

    updated Quicklinks test file

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -27,12 +27,6 @@ export default function QuickLinks() {
 
   const [addLink] = useAddLinkMutation();
   const baseUrl = "https://seabaas.dev.bepeerless.co";
-  // React.useEffect(() => {
-
-  //   return () => {
-  //     // Additional cleanup actions can be performed here
-  //   };
-  // }, []);
 
   useEffect(() => {
     console.log(isLoading, quickLinks)
