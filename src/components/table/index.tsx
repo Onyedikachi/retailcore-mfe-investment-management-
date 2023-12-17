18dd40b0e9c764963f142dd3427d0996c1db082f
TOLULOPE <tolu.oduro@appquest.com.ng>
2023-12-17 17:45:48 +0100

    Added tests and refactored

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -33,6 +33,7 @@ import {
 import Button from "../Button";
 import { ActiveFilterOptions } from "@app/constants";
 import MessagesComponent from "./MessagesComponent";
+import { actionHandler } from "./actionHandler";
 
 interface TableProps {
   headers: any[];
@@ -195,96 +196,9 @@ export default function TableComponent<TableProps>({
 
   const notify = (toastMessage) => toast.error(toastMessage);
   // function getdata(item, key) {}
+  // @ts-ignore
   const handleAction = (action, items) => {
-    setAction(action);
-    setDetail(items);
-    dropDownClick(action, items);
-    setSubText("");
-    previousData.current = {
-      ...previousData.current,
-      productName: items?.productName,
-      prodType: items?.productType,
-      state: items?.state,
-      description: items?.description,
-      slogan: items?.slogan,
-      currency: items?.currency,
-      requestStatus: null,
-      requestType: null,
-      request: "",
-      initiatorId: "",
-      approved_By_Id: "",
-      date: new Date(),
-    };
-
-    if (action.toLowerCase() === Actions.DEACTIVATE) {
-      setConfirmText(Prompts.PRODUCT_DEACTIVATE);
-      setSubText(Prompts.PRODUCT_DEACTIVATE_SUBTEXT);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.ACTIVATE) {
-      setConfirmText(Prompts.PRODUCT_ACTIVATE);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.WITHDRAW_DELETE) {
-      setConfirmText(Prompts.PRODUCT_WITHDRAW_DELETE);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.DELETE_REQUESTS) {
-      setConfirmText(Prompts.PRODUCT_DELETE);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.WITHDARW_MODIFY) {
-      setConfirmText(Prompts.PRODUCT_WITHDRAW_MODIFY);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.MODIFY) {
-      setConfirmText(Prompts.PRODUCT_MODIFY);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.DELETE_DRAFT) {
-      setConfirmText(Prompts.PRODUCT_DELETE);
-      setIsConfirmOpen(true);
-      return;
-    }
-    if (action.toLowerCase() === Actions.CONTINUE_REQUEST) {
-      navigate(
-        `/product-factory/investment/${encodeURIComponent(
-          "term deposit"
-        )}/continue/?id=${items.id}&type=draft&filter=${selected.value}`
-      );
-      return;
-    }
-
-    if (action.toLowerCase() === Actions.VIEW) {
-      category === StatusCategoryType.AllProducts
-        ? setDetailOpen(true)
-        : navigate(
-            `/product-factory/investment/${encodeURIComponent(
-              "term deposit"
-            )}/process-summary/preview/${items.id}?category=request&filter=${
-              selected.value
-            }`
-          );
-      return;
-    }
-    if (action.toLowerCase() === "review") {
-      category === StatusCategoryType.AllProducts
-        ? setDetailOpen(true)
-        : navigate(
-            `/product-factory/investment/${encodeURIComponent(
-              "term deposit"
-            )}/process-summary/verdict/${items.id}?category=request&filter=${
-              selected.value
-            }`
-          );
-      return;
-    }
+    actionHandler({action, items, category, setAction, setDetail, setSubText, dropDownClick, previousData, setConfirmText, setIsConfirmOpen, setDetailOpen, navigate, selected })
   };
 
   const [
