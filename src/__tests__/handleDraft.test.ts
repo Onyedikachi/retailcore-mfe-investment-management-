32d60d6d08b849d51bd98852b0ad13fbbd879f7c
TOLULOPE <tolu.oduro@appquest.com.ng>
2023-12-18 23:00:22 +0100

    added tests and refactors

--- src/__tests__/handleDraft.test.ts
+++ src/__tests__/handleDraft.test.ts
@@ -14,4 +14,79 @@ describe('handleDraft', () => {
 
       expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
     });
+
+    it("should set modifyProduct if process == 'modify'", () => {
+      const setIsConfirmOpen = jest.fn();
+      const productData = {};
+      const process = "modify";
+      const id = "123";
+      const modifyRequest = jest.fn();
+      const modifyProduct = jest.fn();
+      const createProduct = jest.fn();
+
+      handleDraft({ productData, process, id, modifyRequest, setIsConfirmOpen, modifyProduct, createProduct });
+
+      expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+      expect(modifyProduct).toHaveBeenCalledWith({ ...productData, isDraft: true, id });
+    })
+
+    it("should set createProduct if process == 'create'", () => {
+      const setIsConfirmOpen = jest.fn();
+      const process = "create";
+      const id = "123";
+      const modifyRequest = jest.fn();
+      const productData = {id: id};
+      const modifyProduct = jest.fn();
+      const createProduct = jest.fn();
+      
+      handleDraft({ productData, process, id, modifyRequest, setIsConfirmOpen, modifyProduct, createProduct });
+      
+      expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+      expect(createProduct).toHaveBeenCalledWith({ ...productData, isDraft: true, id });
+    })
+    
+    it("should set createProduct if process == 'clone'", () => {
+      const setIsConfirmOpen = jest.fn();
+      const process = "clone";
+      const id = "123";
+      const productData = {id: id};
+      const modifyRequest = jest.fn();
+      const modifyProduct = jest.fn();
+      const createProduct = jest.fn();
+
+      handleDraft({ productData, process, id, modifyRequest, setIsConfirmOpen, modifyProduct, createProduct });
+
+      expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+      expect(createProduct).toHaveBeenCalledWith({ ...productData, isDraft: true, id });
+    })
+
+    it("should set modifyProduct if process == 'continue'", () => {
+      const setIsConfirmOpen = jest.fn();
+      const process = "continue";
+      const id = "123";
+      const productData = {id: id};
+      const modifyRequest = jest.fn();
+      const modifyProduct = jest.fn();
+      const createProduct = jest.fn();
+
+      handleDraft({ productData, process, id, modifyRequest, setIsConfirmOpen, modifyProduct, createProduct });
+
+      expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+      expect(modifyRequest).toHaveBeenCalledWith({ ...productData, isDraft: true, id });
+    })
+
+    it("should set modifyProduct if process == 'withdraw_modify'", () => {
+      const setIsConfirmOpen = jest.fn();
+      const process = "withdraw_modify";
+      const id = "123";
+      const productData = {id: id};
+      const modifyRequest = jest.fn();
+      const modifyProduct = jest.fn();
+      const createProduct = jest.fn();
+
+      handleDraft({ productData, process, id, modifyRequest, setIsConfirmOpen, modifyProduct, createProduct });
+
+      expect(setIsConfirmOpen).toHaveBeenCalledWith(false);
+      expect(modifyRequest).toHaveBeenCalledWith({ ...productData, isDraft: true, id });
+    })
 });
