e0960af08d871e35798c2c5a2ac8bea4890410ec
Success Ahon <40746773+arudovwen@users.noreply.github.com>
2024-01-25 20:35:51 +0100

    Update

--- src/components/Charts/OverviewDonutChartInfo.tsx
+++ src/components/Charts/OverviewDonutChartInfo.tsx
@@ -8,12 +8,12 @@ export default function ChartInfo({labels, title}) {
   return (
     <div className="rounded-[5px] bg-white px-5 py-6 shadow-custom h-full">
       <div className="flex items-center justify-between border-b border-[#ccc] pb-1">
-        <span className="text-base font-semibold text-[#636363]">
+        <span className="text-base font-semibold text-[#636363] whitespace-nowrap">
           All Investments {title}
         </span>
 
         <button className="flex h-[32px] items-center gap-[8px] rounded-[6px] bg-transparent px-3 py-[4px] text-[#8F8F8F] ">
-          <span className="text-sm  text-[#8F8F8F]">Filter by Date</span>
+          <span className="text-sm  text-[#8F8F8F] whitespace-nowrap">Filter by Date</span>
 
           <DateSelect
             onChangeDate={(value) => {
@@ -28,14 +28,14 @@ export default function ChartInfo({labels, title}) {
         </button>
       </div>
 
-      <div className="mt-[15px] flex items-center justify-between">
+      <div className="mt-[15px] flex items-center justify-between gap-x-6">
         <div className="flex items-center gap-[9px]">
           <span className="text-xs font-normal text-[#4F4F4F]">NGN</span>
           <span className="text-[20px] font-medium text-[#636363]">
-            100,000,000.00
+            100,000,000
           </span>
         </div>
-        <div className="text-xs font-normal text-[#8F8F8F]">
+        <div className="text-xs font-normal text-[#8F8F8F] whitespace-nowrap">
           Sept, 2022 till date
         </div>
       </div>
